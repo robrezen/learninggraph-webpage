8bc2abe76a06468944e05c170da3d0b6f3cbc198
Robson Rezende <85044649+robrezen@users.noreply.github.com>
2024-06-18 00:47:30 -0300

    Add basic homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about/about.component';
+import { HomeComponent } from './features/about/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
